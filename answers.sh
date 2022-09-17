b4c4c05f864216a18b470e8aca9eb59ceb11262d
ve345 <112516697+ve345@users.noreply.github.com>
2022-09-16 23:54:31 -0400

    Update answers.sh

--- answers.sh
+++ answers.sh
@@ -4,37 +4,37 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv Memes/'Star Wars'/fafsa.jpg Memes/Spongebob/fafsa.jpg
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-
+cp -r Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv Programs/Count.c Programs/Goodbye.c Programs/C
 
 # Task 9
-
+mv Programs/Name.py Programs/Space.py Programs/'Python 3'
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls ..
 
 # Task 12
 : '
